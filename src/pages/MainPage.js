b0ce559ea47a012fb2b22bab3044fea57ac73f10
dia eddine <diaeddinee@outlook.fr>
2024-07-04 17:11:45 +0100

    Added the edit and delete rule's option and made the code better

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,61 +1,216 @@
 import React, { useState, useEffect } from "react";
 import SearchRule from "../components/BusnessRuleDisplay/SearchRule";
-import Popup from "../components/CreateRule/CreateRules";
-import { Dropdown, Menu } from "antd";
 
-const items = [
-  {
-    label: "Delete",
-    key: "1",
-  },
-  {
-    label : "Update",
-    key : "2",
-  }
-];
+import { Dropdown, Menu, notification } from "antd";
+import { NotificationContainer, NotificationManager } from "react-notifications";
+import axios from "axios";
+
+import { SmileOutlined } from '@ant-design/icons';
 
 function MainPage() {
+  
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (description,message) => {
+    api.open({
+      message: message,
+    });}
+  
   const [showPopup, setShowPopup] = useState(false);
-  const [loadings, setLoadings] = useState([]);
   const [rules, setRules] = useState([]);
-  const [loading, setLoading] = useState(true);
+
+  const [rule,setRule]=useState({
+    id:null,
+    Name:"",
+    Description:"",
+    Condition:"",
+    Action:"",
+    Modify:false
+  })
+ 
+
+  const resetRule=()=>{
+    setRule({
+      id:null,
+      Name: "",
+      Description: "",
+      Condition: "",
+      Action: "",
+      Modify:false
+    });
+  }
+
+  const handleChange = (e)=>{
+    const name=e.target.name
+    setRule(prev=>({...prev,[name]:e.target.value}))
+  }
+
+  const createRule = async (e) => {
+    // stop the form from reloading the page
+    e.preventDefault();
+    try {
+      if(rule.Modify){
+        const response = await axios.put(`http://127.0.0.1:8000/api/update_rule/${rule.id}`,{
+          ruleName: rule.Name,
+          description: rule.Description,
+          condition: rule.Condition,
+          action: rule.Action,
+
+        })
+        resetRule()
+        openNotification("success","Rule Modified successfully")
+        //NotificationManager.success('Rule Modified successfully');
+        fetchRules()
+        //setShowPopup(false)
+        return
+      }
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/create_rule",
+        {
+          ruleName: rule.Name,
+          description: rule.Description,
+          condition: rule.Condition,
+          action: rule.Action,
+        }
+      );
+      resetRule()
+      fetchRules()
+      openNotification("success","Rule created successfully")
+      //NotificationManager.success('Rule created successfully');
+      //setShowPopup(false)
+      //onClose(); 
+    } catch (err) {
+      //NotificationManager.error("Rule not created");
+    }
+  };
+
+
+  
+
+  const fetchRules = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/list_rule");
+      const data = await response.json();
+      setRules(data);
+      //setLoading(false);
+    } catch (error) {
+      console.error("Error fetching rules:", error);
+      //setLoading(false);
+    }
+  };
+
+  const deleterule = async (id) => {
+    try {
+      await fetch(`http://127.0.0.1:8000/api/delete_rule/${id}`, {
+        method: "DELETE"
+      });
+      fetchRules(); // Refresh rules after deletion
+    } catch (error) {
+      console.error("Error deleting rule:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchRules = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/api/list_rule/");
-        const data = await response.json();
-        setRules(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching rules:", error);
-        setLoading(false);
-      }
-    };
-
     fetchRules();
   }, []);
 
-  const enterLoading = (index) => {
-    setLoadings((state) => {
-      const newLoadings = [...state];
-      newLoadings[index] = true;
-      return newLoadings;
-    });
-    setTimeout(() => {
-      setLoadings((state) => {
-        const newLoadings = [...state];
-        newLoadings[index] = false;
-        return newLoadings;
-      });
-    }, 6000);
-  };
 
-  const togglePopup = () => {
-    setShowPopup(!showPopup);
-  };
+
+ 
 
   return (
+    <>
+    {contextHolder}
+
+     <div className={`fixed inset-0 flex items-center justify-center z-50 ${showPopup ? '' : 'hidden'}`}>
+
+      <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
+      <div className="bg-white p-8 rounded-lg shadow-lg z-50 relative w-1/4">
+        <h2 className="text-xl mb-4 text-center">
+        <b>{rule.Modify ? 'Modify Rule' : 'Create Rule'}</b>
+        </h2>
+        <button className="absolute top-0 right-0 mt-4 mr-4" onClick={()=>{setShowPopup(false);resetRule()}}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <form onSubmit={createRule}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Rule Name
+            </label>
+            <input
+              type="text"
+              name="Name"
+              value={rule.Name}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Description
+            </label>
+            <textarea
+              name="Description"
+              value={rule.Description}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Condition
+            </label>
+            <input
+              type="text"
+              name="Condition"
+              value={rule.Condition}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Action
+            </label>
+            <input
+              type="text"
+              name="Action"
+              value={rule.Action}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="mr-2 bg-blue-700 text-white px-4 py-2 rounded-md"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md"
+              onClick={()=>setShowPopup(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+      <NotificationContainer />
+    </div>
     <div className="">
       <SearchRule />
       <div className="w-full">
@@ -82,7 +237,7 @@ function MainPage() {
             <button
               type="button"
               className="flex items-center text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
-              onClick={togglePopup}
+              onClick={() => setShowPopup(true)}
             >
               <svg
                 className="w-4 h-4 mr-2"
@@ -96,7 +251,6 @@ function MainPage() {
             </button>
           </div>
         </div>
-        {showPopup && <Popup show={showPopup} onClose={togglePopup} />}
 
         <div className="Table mt-8 ml-28 mr-5">
           <div className="relative overflow-x-auto bg-white border border-gray-300 rounded-lg">
@@ -125,63 +279,55 @@ function MainPage() {
                   <th scope="col" className="px-6 py-3">
                     Action
                   </th>
+                  <th scope="col" className="px-6 py-3">
+                    Options
+                  </th>
                 </tr>
               </thead>
               <tbody>
-                  {rules.map((rule) => (
-                    <tr key={rule.id} className="bg-white border-b">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                      >
-                        <div className="flex items-center mb-4">
-                          <input
-                            id="default-checkbox"
-                            type="checkbox"
-                            value=""
-                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                          />
-                        </div>
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                      >
-                        {rule.ruleName}
-                      </th>
-                      <td className="px-6 py-4">{rule.description}</td>
-                      <td className="px-6 py-4">{rule.condition}</td>
-                      <td className="px-6 py-4 relative">
-                        {rule.action}
-                        <Dropdown
-                          overlay={<Menu items={items} />}
-                          trigger={["click"]}
-                        >
-                          <button type="button" className="absolute right-0 p-2">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-5 w-5"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M2 9a2 2 0 100-4 2 2 0 000 4zM8 9a2 2 0 100-4 2 2 0 000 4zm6 0a2 2 0 100-4 2 2 0 000 4z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </button>
-                        </Dropdown>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
+                {rules.map((rule) => (
+                  <tr key={rule.id} className="bg-white border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      <div className="flex items-center mb-4">
+                        <input
+                          id="default-checkbox"
+                          type="checkbox"
+                          value=""
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </th>
+                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{rule.ruleName}</td>
+                    <td className="px-6 py-4">{rule.description}</td>
+                    <td className="px-6 py-4">{rule.condition}</td>
+                    <td className="px-6 py-4 relative">{rule.action}</td>
+                    <td>
+                      <button className="mr-2 p-4 border-2 border-indigo-500 rounded-xl hover:bg-blue-500 hover:text-black" onClick={() =>{ setShowPopup(true); // Show the popup // Set the selected rule ID
+                        setRule({
+                          "id":rule.id,
+                          "Name":rule.ruleName,
+                          "Description":rule.description,
+                          "Condition":rule.condition,
+                          "Action":rule.action,
+                          "Modify":true
+                      })
+                      }}>Modify</button>
+                      <button className="p-4 border-2 border-indigo-500 rounded-xl hover:bg-red-500 hover:text-black" onClick={() => deleterule(rule.id)}> DELETE</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
           </div>
         </div>
       </div>
+
+
     </div>
+    </>
   );
 }
-
 export default MainPage;
